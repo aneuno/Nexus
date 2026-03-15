df1f01d6e2c0b2493ce1e8dd47224aa1bf03b8e4
aneuno <155757270+aneuno@users.noreply.github.com>
2026-03-15 20:57:15 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,4 @@
-import type { Metadata } from 'next'
 import './globals.css'
-
-export const metadata: Metadata = {
-  title: 'Nexus Chronicles',
-  description: 'Multiverse Anime Card Game',
-}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
